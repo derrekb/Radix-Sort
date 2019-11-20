b0f080faf6f532e31c552258def7d137693673de
derrekb <45970048+derrekb@users.noreply.github.com>
2019-11-19 19:28:08 -0500

    Add files via upload

--- Test.java
+++ Test.java
@@ -6,7 +6,13 @@ class Test {
 
 	@org.junit.jupiter.api.Test
 	void test() {
-		fail("Not yet implemented");
+		int x= 1234;
+	    int y= 9;
+	    Integer[] z= {225,450,666,777,1295869};
+
+	    assertEquals( Radix.nthDigit(x,  1), 4 );
+	    assertEquals(Radix.getLength(9),1);
+	    assertEquals(Radix.getLargest(z,666,4),3);
 	}
 
 }
