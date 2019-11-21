cbcd628bccd56def394e19afbbf21bb2be1b8987
Emanoid <56281174+Emanoid@users.noreply.github.com>
2019-11-21 15:40:52 -0500

    Update Radix.java

--- Radix.java
+++ Radix.java
@@ -13,55 +13,41 @@ public class Radix {
     public static int getLength(int n) {
     	
     	if(n >= 0)
-        {return String.valueOf(n).length();}
-    	
+    		{return String.valueOf(n).length();}
     	else
-    	{return String.valueOf(n).length()-1;}
-    }
-
-
-    //Purpose: To calculate the length of the longest number in a vector of
+    		{return String.valueOf(n).length()-1;}    }
+  
    //Purpose: To calculate the length of the longest number in a vector of
-     //         natural numbers.
- public static int getLargest(int[] n, int index) {
-     int accum = 0;
-	 if (index == -1) return accum;
-     else if (accum < getLength(n[index])) {
-         accum = getLength(n[index]);
-    	 return accum;
-     }
-     else return getLargest(n, index-1);
- }
+   //natural numbers.
+    public static int getLargest(int[] n, int index) {
+	     int accum = 0;
+		 if (index == -1)
+		 	{return accum;}
+	     else if (accum < getLength(n[index])) {
+	         accum = getLength(n[index]);
+	    	 return accum;	     }
+	     else return getLargest(n, index-1); }
  
- //Purpose: To calculate the length of the shortest number in a vector of
-//Purpose: To calculate the length of the shortest number in a vector of
-  //         natural numbers.
-public static int getSmallest(int[] n, int index) {
-	int accum = getLargest(n, n.length-1); 
-  if (index == -1) return accum;
-  else if (accum > getLength(n[index])) {
-      accum = getLength(n[index]);
-      return accum;
-  }
-  else return getSmallest(n, index-1);
-}
+     //Purpose: To calculate the length of the shortest number in a vector of
+    //natural numbers.
+    public static int getSmallest(int[] n, int index) {
+		int accum = getLargest(n, n.length-1); 
+		if (index == -1) 
+			{return accum;}
+		else if (accum > getLength(n[index])) {
+			accum = getLength(n[index]);
+			return accum;		}
+		else {return getSmallest(n, index-1);}	}
 
-
-
-
-
-//to create a random array with a random size with random values
-public static int[] randomArr() {
-	
-	int index = 0;
-	int a = 10 + (int) (Math.random()*30);// just to get a decent sample size
-	int[] arr = new int[a];
-	
-while(index <= (a - 1)) {
-	
-	arr[index] = (int)Math.floor(Math.random()*100);
-	index++;
-}
-return(arr);
-}
+    //Purpose: To create a random array with a random size with random values
+    public static int[] randomArr() {
+		int index = 0;
+		int a = 10 + (int) (Math.random()*30);
+		// just to get a decent sample size
+		int[] arr = new int[a];	
+		while(index <= (a - 1)) {
+			arr[index] = (int)Math.floor(Math.random()*100);
+			index++;}
+			return(arr);}
+    
 }
