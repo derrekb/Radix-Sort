81cb17eca8f5074eb0288fff83741744a3407fc6
Emanoid <56281174+Emanoid@users.noreply.github.com>
2019-11-21 18:32:35 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,27 +1,48 @@
-package radixSort;
+package RADIX_SORT;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-class Test {
+class Radix_Sort_Test {
 
-	@org.junit.jupiter.api.Test
-	void test() {
-	    int x= 1234;
-	    int x1 = -1234;
+	@Test
+	void Radix() {
+		int x= 1234;
 	    int y= 9;
-	    int y1 = -9;
-	    int [] z= {284, 7, 13020,6,65,3};
-	    int[] z1 = {-1,1,2,-2,33,3,-8};
-	    int[] z2 = {};
+	    int [] z= {225,450,666,777,1295869};
+	    int [] p= {-5,450,666,777,1295869};
 
-	    assertEquals( Radix.nthDigit(x,  1), 4);
-	    assertEquals( Radix.nthDigit(x1,  1), -4);
-	    assertEquals(Radix.getLength(y),1);
-	    assertEquals(Radix.getLength(y1),1);
-	    assertEquals(Radix.getLargest(z,0,5),5);
-	    assertEquals(Radix.getLargest(z1,0,6),2);
-	    //assertEquals(Radix.getLargest(z2,0,0),0);  needs to work with empty array
-	    assertEquals(Radix.getSmallest(z, 0, 5),1);
+	    assertEquals(Radix.nthDigit(x,1), 4 );
+	    assertEquals(Radix.getLength(9),1);
+	    assertEquals(Radix.getLargest(z,4),7);
+	    assertEquals(Radix.getSmallest(z,4),3);
+	 //   assertEquals(Radix.getSmallest(p,4),1);
+	  
+	    }
+	
+	@Test
+	void Bucket() {
+		IBucket B= new Bucket();
+		B.add(0);
+		B.add(1);
+		B.add(2);
+		B.add(3);
+		int X[] = {0,0,0,0};
+		assertEquals(B.toArray(X, 0) , 4);
+		//assertEquals(X, new Integer[] {0,1,2,3});
+		//assertEquals(B.size, 0); //Need selector to test this
+	}
+	
+	@Test
+	void Buckets() {
+		Buckets b = new Buckets();
+		int [] z= {225,450,666,777,1295869};
+		b.add(z, 1);
+		assertEquals(b.five.get(0), 225);
+		assertEquals(b.zero.get(0), 450);
+		assertEquals(b.six.get(0), 666);
+		assertEquals(b.seven.get(0), 777);
+		assertEquals(b.nine.get(0), 1295869);
 	}
 
 }
