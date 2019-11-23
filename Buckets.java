5df5acf2f1de42725328f8dd29f22ad3bcf91a8e
Emanoid <56281174+Emanoid@users.noreply.github.com>
2019-11-23 14:57:16 -0500

    Add files via upload

---
+++ Buckets.java
@@ -0,0 +1,189 @@
+package RADIX_SORT;
+
+/**
+ *Class for Buckets
+ *Variables: [minusnine,minuseight,...,minuszero,zero,one,two,....,nine]
+ *			 ==> Buckets for values with an nthDigit of [-9,-8,.......,9] 
+ *				respectively
+ */
+public class Buckets implements IBuckets{
+	//Variables
+	IBucket zero,one,two,three,four,five,six,seven,eight,nine;
+	IBucket minusnine,minuseight,minusseven,minussix,minusfive;
+	IBucket minusfour,minusthree,minustwo,minusone,minuszero;
+	
+	//Purpose: Implements	 a Buckets
+	//Buckets ==> A Buckets is a collection of Bucket(s) 
+	public Buckets() {
+		minusnine = new Bucket();
+		minuseight = new Bucket();
+		minusseven = new Bucket();
+		minussix = new Bucket();
+		minusfive = new Bucket();
+		minusfour = new Bucket();
+		minusthree = new Bucket();
+		minustwo = new Bucket();
+		minusone = new Bucket();
+		minuszero = new Bucket();
+		zero = new Bucket();
+		 one = new Bucket();
+		 two = new Bucket();
+		 three = new Bucket();
+		 four = new Bucket();
+		 five = new Bucket();
+		 six = new Bucket();
+		 seven = new Bucket();
+		 eight = new Bucket();
+		 nine = new Bucket();	}
+
+	//Purpose: To add items in an array into this Buckets
+		public void add(int[] n, int index) {
+			//n ==> Inputed array from user
+			//index ==> nth digit of an element in array n
+			//i ==> index of array n
+			int i = 0;
+			//INV: this.Bucket[zero[n[i-1] if (nthDigit(n[i-1],index)=0)],
+			     //one[n[i-1] if (nthDigit(n[i-1],index)=1)]
+				//	,...,nine[n[i-1] if (nthDigit(n[i-1], index)= 9)] ,
+			    //0<=i<=n.length, i => i
+			while(i< n.length) {
+				//INV: this.Bucket[zero[n[i-1] if (nthDigit(n[i-1],index)=0)],
+			     //one[n[i-1] if (nthDigit(n[i-1],index)=1)]
+				//	,...,nine[n[i-1] if (nthDigit(n[i-1], index)= 9)] ,
+			    //0<=i<=n.length, i => i
+				//conditions starts
+				if(Radix.nthDigit(n[i], index) == 0) {
+					zero.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == 1) {
+					one.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == 2) {
+					two.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == 3) {
+					three.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == 4) {
+					four.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == 5) {
+					five.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == 6) {
+					six.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == 7) {
+					seven.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == 8) {
+					eight.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == 9){
+					nine.add(n[i]);} 
+				else if(Radix.nthDigit(n[i], index) == -10) {
+					minusnine.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == -9) {
+					minuseight.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == -8) {
+					minusseven.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == -7) {
+					minussix.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == -6) {
+					minusfive.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == -5) {
+					minusfour.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == -4) {
+					minusthree.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == -3) {
+					minustwo.add(n[i]);	}
+				else if(Radix.nthDigit(n[i], index) == -2) {
+					minusone.add(n[i]);	}
+				else {minuszero.add(n[i]);	}
+				//conditions ends
+				//INV: this.Bucket[zero[n[i] if (nthDigit(n[i],index)=0)],
+			     //one[n[i] if (nthDigit(n[i],index)=1)]
+				//	,...,nine[n[i] if (nthDigit(n[i], index)= 9)] ,
+			    //0<=i<=n.length, i == i
+				i++;	
+				//INV: this.Bucket[zero[n[i-1] if (nthDigit(n[i-1],index)=0)],
+			     //one[n[i-1] if (nthDigit(n[i-1],index)=1)]
+				//	,...,nine[n[i-1] if (nthDigit(n[i-1], index)= 9)] ,
+			    //0<=i<=n.length, i == i+1
+				//Which implies:
+				//INV: this.Bucket[zero[n[i-1] if (nthDigit(n[i-1],index)=0)],
+			     //one[n[i-1] if (nthDigit(n[i-1],index)=1)]
+				//	,...,nine[n[i-1] if (nthDigit(n[i-1], index)= 9)] ,
+			    //0<=i<=n.length, i == i
+				}
+			//INV: this.Bucket[zero[n[i] if (nthDigit(n[i],index)=0)],
+		     //one[n[i] if (nthDigit(n[i],index)=1)]
+			//	,...,nine[n[i] if (nthDigit(n[i], index)= 9)] ,
+		    //0<i<=n.length, i == n.length
+			/**Termination Argument: i starts at 0 and increments each time
+			 * 						through the loop. Eventually i = n.length
+			 * 						and the loop terminates 
+			 */
+		}
+
+	//Purpose: To dump elements in this Buckets to array A at index i
+		public void toArray(int[] A, int i) {
+			//i==> index in Array 
+			minusnine.toArray(A, i);
+			i = minusnine.SIZE();
+			minuseight.toArray(A, i);
+			i = i + minuseight.SIZE();
+			minusseven.toArray(A, i);
+			i = i + minusseven.SIZE();
+			minussix.toArray(A, i);
+			i = i + minussix.SIZE();
+			minusfive.toArray(A, i);
+			i = i + minusfive.SIZE();
+			minusfour.toArray(A, i);
+			i = i + minusfour.SIZE();
+			minusthree.toArray(A, i);
+			i = i + minusthree.SIZE();
+			minustwo.toArray(A, i);
+			i = i + minustwo.SIZE();
+			minusone.toArray(A, i);
+			i = i + minusone.SIZE();
+			minuszero.toArray(A, i);
+			i = i + minuszero.SIZE();
+			zero.toArray(A, i);
+			i = i + zero.SIZE();
+			one.toArray(A, i);
+			i = i + one.SIZE();
+			two.toArray(A, i);
+			i = i + two.SIZE();
+			three.toArray(A, i);
+			i = i + three.SIZE();
+			four.toArray(A, i);
+			i = i + four.SIZE();
+			five.toArray(A, i);
+			i = i + five.SIZE();
+			six.toArray(A, i);
+			i = i + six.SIZE();
+			seven.toArray(A, i);
+			i = i + seven.SIZE();
+			eight.toArray(A, i);
+			i = i + eight.SIZE();
+			nine.toArray(A, i);			}
+		
+	//Purpose: To empty all elements in this Buckets
+	//Effect: Empties all Buckets contained in this Buckets
+	public void CLEAR() {
+		minusnine.CLEAR();
+        minuseight.CLEAR();
+        minusseven.CLEAR();
+        minussix.CLEAR();
+        minusfive.CLEAR();
+        minusfour.CLEAR();
+        minusthree.CLEAR();
+        minustwo.CLEAR();
+        minusone.CLEAR();
+        minuszero.CLEAR();
+		zero.CLEAR();
+		one.CLEAR();
+		two.CLEAR();
+		three.CLEAR();
+		four.CLEAR();
+		five.CLEAR();
+		six.CLEAR();
+		seven.CLEAR();
+		eight.CLEAR();
+		nine.CLEAR();		}
+	
+}
+
+	
---
+++ Radix.java
@@ -0,0 +1,128 @@
+package RADIX_SORT;
+import java.util.Arrays;
+
+/**
+ * Class for Radix_Sort
+  */
+public class Radix {
+	//Purpose: To return the digit at the given index of the input number,
+    //Effect: getDigit(1234, 1) would return 4, getDigit(1234, 2) would return 3
+    //         and getDigit(5, 3) would return 0
+    public static int nthdigit(int n, int index) {
+    	return Math.floorDiv((n % ((int) Math.pow(10, index))), ((int) Math.pow(10, index-1)));}
+    
+    public static int nthDigit(int n, int index) {
+    	if(n<0 && (nthdigit(n, index)== 0)) {return 100;}
+    	else {return nthdigit(n, index);}
+    }
+
+    //Purpose: To get the length of the given int n.
+    //Example: 21 would return 2, 5 would return 1.
+    public static int getLength(int n) {
+    	if(n >= 0)
+    		{return String.valueOf(n).length();}
+    	else
+    		{return String.valueOf(n).length()-1;}    }
+  
+   //Purpose: To calculate the length of the longest number in a vector of
+   //natural numbers.
+    public static int getLargest(int[] n, int index) {
+	     int accum = 0;
+		 if (index == -1)
+		 	{return accum;}
+	     else if (accum < getLength(n[index])) {
+	         accum = getLength(n[index]);
+	    	 return accum;	     }
+	     else return getLargest(n, index-1); }
+ 
+   //Purpose: To sort Array A starting from index
+   public static int[] sort(int[] A, int index) {
+	    //To obtain element in A with largest digit size
+    	int largest = getLargest(A, A.length-1);
+    	//Bucket to add elements and dump elements from
+    	IBuckets b = new Buckets();
+    	//Assumption: y == elements in Array A
+    	//i ==> nth-digit of y
+    	int i = 1;
+    	//Assumption: y == elements in Array A, y == A[0 .... A.length-1]
+    	
+    	//A[y] ,where y is sorted in ascending order of value of
+		//		value of nth digit(s) of y &&
+		//i = i  &&
+    	//b[] && 
+		//nth-digit ==> i - 1
+    	while(i<= (largest+2)) {
+    		//A[y] ,where y is sorted in ascending order of value of
+    		//		value of nth digit(s) of y &&
+    		//i = i  &&
+        	//b[] && 
+    		//nth-digit => i - 1
+    		b.add(A, i);
+    		//A[y] , where y is not sorted &&
+        	//i = i && 
+    		//b[y], where y is sorted in ascending order of 
+    		//				value of the nth digit(s) of y && 
+    		//nth-digit ==> i 
+    		b.toArray(A, index);
+    		//A[y] ,where y is sorted in ascending order of value of
+    		//		value of nth digit(s) of y && 
+    		//i = i &&
+        	//b[y], where y is sorted in ascending order of 
+    		//		value of the nth digit(s) of y && 
+    		//nth-digit ==> i 
+    		b.CLEAR();
+    		//A[y] ,where y is sorted in ascending order of value of
+    		//		value of nth digit(s) of y &&
+    		//i = i &&
+        	//b[] && 
+    		//nth-digit ==> i 
+    		i++;  	   
+    		//A[y] ,where y is sorted in ascending order of value of
+    		//		value of nth digit(s) of y &&
+    		//i = i + 1 &&
+        	//b[] && 
+    		//nth-digit ==> i - 1
+    		//==> Which implies:
+    		//A[y] ,where y is sorted in ascending order of value of
+    		//		value of nth digit(s) of y &&
+    		//i = i &&
+        	//b[] && 
+    		//nth-digit ==> i - 1
+    	}
+    	//A[y] ,where y is completely sorted in ascending order &&
+		//i = largest + 2 &&
+    	//b[] && 
+		//nth-digit ==> i - 1
+    	/**Termination Argument: i starts at 1 and increments each time
+    	 * 						through the loop. Eventually, i > (largest+1)
+    	 * 						and the loop terminates  
+    	 */
+    	return A;      	  }
+   
+   //Purpose: To generate a random Array with random size
+   public static int[] randomArr() {
+		int index = 0;
+		int a = (int) (Math.random()*30);
+		// To generate a decent sample size
+		int[] arr = new int[a];	
+		//To implement an array arr with size a
+		//Assumption: let y be the values of randomly generated integers
+		//INV: arr[0...index-1] && index = index
+		while(index <= (a - 1)) {
+			//INV: arr[0....index-1] && index = index
+			arr[index] = (int)Math.floor(Math.random()*-1000);
+			//INV: arr[0....index] && index = index
+			index++;
+			//INV: arr[0....index-1] && index = index + 1
+			//which implies:
+			//INV: arr[0....index-1] && index = index
+		}
+		/**Termination Argument: index starts zero and increments each time
+		 * 						through the loop. Eventually index > a-1. And
+		 * 						the loop terminates
+		 */		
+			return(arr);}
+   
+   
+   
+ }
