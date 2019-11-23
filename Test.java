ed0ec8fff76827275b35dc9f7e703c7fb79c122f
Emanoid <56281174+Emanoid@users.noreply.github.com>
2019-11-23 12:39:31 -0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package RADIX_SORT;
+
+import java.util.Arrays;
+
+public class Test {
+	public static void main(String args[]) {
+		int[] A = Radix.randomArr();
+		System.out.println("Array before sorting with Radix");
+		System.out.println(Arrays.toString(A));
+		System.out.println("Array after sorting with Radix");
+		System.out.println(Arrays.toString(Radix.sort(A, 0)));
+		
+	}
+}
+	
